a47723d098ef071b35138cf5f19a6c3105c464dd
Nick Such <nicksuch@gmail.com>
2015-12-01 19:16:56 -0500

    Initial commit

--- Space Invaders/GameScene.swift
+++ Space Invaders/GameScene.swift
@@ -8,14 +8,16 @@
 
 import SpriteKit
 
+var isSpaceship = true
+
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
+        myLabel.text = "Space Invaders"
+        myLabel.fontSize = 45
+        myLabel.position = CGPoint(x: 500, y: 10)
+        print(CGRectGetMidX(self.frame), self.frame.size.height - 55)
         self.addChild(myLabel)
     }
     
@@ -25,13 +27,24 @@ class GameScene: SKScene {
         for touch in touches {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            var sprite = SKSpriteNode(imageNamed:"Spaceship")
+            
+            if (isSpaceship == true) {
+                isSpaceship = false
+            } else {
+                sprite = SKSpriteNode(imageNamed:"Pancakes")
+                isSpaceship = true
+            }
             
             sprite.xScale = 0.5
             sprite.yScale = 0.5
             sprite.position = location
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+//            let action = SKAction.rotateByAngle(CGFloat(2*M_PI), duration:1)
+//            let action = SKAction.scaleBy(2, duration: 1)
+            
+            let action = SKAction.moveByX(CGFloat(0), y: CGFloat(50), duration: 1)
+            
             
             sprite.runAction(SKAction.repeatActionForever(action))
             
